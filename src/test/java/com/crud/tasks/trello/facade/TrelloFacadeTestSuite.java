8639038d4d1e0a86597dca58f33ea73e614c4d52
sokolx <89soek@gmail.com>
2023-03-23 21:56:10 +0100

    21.2.2 add test createCard TrelloFacadeTestSuite

--- src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTestSuite.java
+++ src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTestSuite.java
@@ -1,9 +1,6 @@
 package com.crud.tasks.trello.facade;
 
-import com.crud.tasks.domain.TrelloBoard;
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.domain.TrelloList;
-import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.domain.*;
 import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.validator.TrelloValidator;
@@ -16,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
@@ -24,13 +22,10 @@ class TrelloFacadeTestSuite {
 
     @InjectMocks
     private TrelloFacade trelloFacade;
-
     @Mock
     private TrelloService trelloService;
-
     @Mock
     private TrelloValidator trelloValidator;
-
     @Mock
     private TrelloMapper trelloMapper;
 
@@ -63,7 +58,46 @@ class TrelloFacadeTestSuite {
                 assertThat(trelloListDto.getName()).isEqualTo("test-list");
                 assertThat(trelloListDto.isClosed()).isFalse();
             });
-
         });
+    }
+
+    @Test
+    void shouldFetchEmptyList() {
+        // Given
+        List<TrelloListDto> trelloLists = List.of(new TrelloListDto("1", "test_list", false));
+        List<TrelloBoardDto> trelloBoards = List.of(new TrelloBoardDto("1", "test", trelloLists));
+        List<TrelloList> mappedTrelloLists = List.of(new TrelloList("1", "test_list", false));
+        List<TrelloBoard> mappedTrelloBoards = List.of(new TrelloBoard("1", "test", mappedTrelloLists));
+
+        when(trelloService.fetchTrelloBoards()).thenReturn(trelloBoards);
+        when(trelloMapper.mapToBoards(trelloBoards)).thenReturn(mappedTrelloBoards);
+        when(trelloMapper.mapToBoardsDto(anyList())).thenReturn(List.of());
+        when(trelloValidator.validateTrelloBoards(mappedTrelloBoards)).thenReturn(List.of());
+
+        // When
+        List<TrelloBoardDto> trelloBoardDtos = trelloFacade.fetchTrelloBoards();
+
+        // Then
+        assertNotNull(trelloBoardDtos);
+        assertEquals(0, trelloBoardDtos.size());
+    }
+
+    @Test
+    void shouldCreateNewCard() {
+        // Given
+        TrelloCardDto trelloCardDto = new TrelloCardDto("TrelloCardDtoName", "TrelloCardDtoDescription", "TrelloCardDtoPos", "TrelloCardDtoListId");
+        TrelloCard trelloCard = new TrelloCard("TrelloCardDtoName", "TrelloCardDtoDescription", "TrelloCardDtoPos", "TrelloCardDtoListId");
+        CreatedTrelloCardDto createdTrelloCardDto = new CreatedTrelloCardDto("id", "name", "url.com", null);
+        when(trelloMapper.mapToCard(trelloCardDto)).thenReturn(trelloCard);
+        when(trelloValidator.validateCard(trelloCard)).thenReturn(false);
+        when(trelloService.createTrelloCard(trelloMapper.mapToCardDto(trelloCard))).thenReturn(createdTrelloCardDto);
+
+        // When
+        CreatedTrelloCardDto result = trelloFacade.createCard(trelloCardDto);
+
+        // Then
+        assertEquals("id", result.getId());
+        assertEquals("name", result.getName());
+        assertEquals("url.com", result.getShortUrl());
     }
 }
