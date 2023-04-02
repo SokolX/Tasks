ad59395de9039e417600018ba98d415067d18c0f
sokolx <89soek@gmail.com>
2023-04-02 10:55:25 +0200

    24.3.1 added example iteration function in thymeleaf

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -20,14 +23,22 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
+        context.setVariable("is_friend", true);
         context.setVariable("message", message);
         context.setVariable("tasks_url", appConfig.getAppUrl());
         context.setVariable("button", appConfig.getAppRedirectText());
-        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("show_button", false);
+        context.setVariable("admin_config", adminConfig);
         context.setVariable("goodbye_message", appConfig.getGoodByeMessage());
         context.setVariable("company_name", appConfig.getCompanyName());
         context.setVariable("company_goal", appConfig.getCompanyGoal());
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
